947decd1726efe1737354eae41ea7156bc8321b6
Francisca <franciscabelen112002@gmail.com>
2020-07-27 12:43:26 -0400

    prueba mostrando archivo readme

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+  
+let fs = require('fs');
+
+fs.readFile('README.md', 'utf-8', (error, data) => {
+  if(error) {
+    console.log('error: ', error);
+  } else {
+    console.log(data);
+  }
+});
