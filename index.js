0f590788e0316345433caaa551ce3a97070c4426
Francisca <franciscabelen112002@gmail.com>
2020-07-28 12:58:48 -0400

    mostrando archivos md

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 //   // ...
 // };
 
-/*   
+const fs = require("fs");
+const path = require('path');   
 
-//Lee el archivo README o el archivo que se le indique
+//AQUÍ SE LEEN LOS ARCHIVOS, EN ESTE CASO README (lee el archivo que se le indique)
 
 /* let fs = require('fs');
 fs.readFile('README.md', 'utf-8', (error, data) => {
@@ -16,16 +17,33 @@ fs.readFile('README.md', 'utf-8', (error, data) => {
 }); */
 
 
-// MUESTRA TODOS LOS ARCHIVOS
-
-const fs = require("fs");
+//AQUI SE MUESTRAN TODOS LOS ARCHIVOS MD (intentar mostrar txt igual)
+ 
 fs.readdir('./', (error, files) => {
 
   if(error){
     throw error;
   }
-  console.log(files);
-});
+  else{
+    files.forEach(file => {
+      if(path.extname(file) === ".md"){
+        console.log("Estos son los archivos de extensión md: ", file);
+      }
+    }) 
+  }
+
+  
+});   
+
+//AQUI SE INTENTA LEER LOS ARCHIVOS
+
+fs.readFile('file', 'utf-8', (error, data) => {
+  if(error) {
+    console.log('error: ', error);
+  } else {
+    console.log(data);
+  }
+}); 
  
 
 
