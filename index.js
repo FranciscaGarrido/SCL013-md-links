844e70fa4d9e01f53fa166c4d4f1e163c885afbb
Francisca <franciscabelen112002@gmail.com>
2020-07-28 10:51:25 -0400

    mostrando todos los archivos

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // module.exports = () => {
 //   // ...
 // };
-  
-let fs = require('fs');
 
+/*   
+
+//Lee el archivo README o el archivo que se le indique
+
+/* let fs = require('fs');
 fs.readFile('README.md', 'utf-8', (error, data) => {
   if(error) {
     console.log('error: ', error);
   } else {
     console.log(data);
   }
+}); */
+
+
+// MUESTRA TODOS LOS ARCHIVOS
+
+const fs = require("fs");
+fs.readdir('./', (error, files) => {
+
+  if(error){
+    throw error;
+  }
+  console.log(files);
 });
+ 
+
+
+
+
+
+
+
